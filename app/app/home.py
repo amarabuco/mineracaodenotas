0ba13183ef3347779f28a8ed30172a7e3dd6c41c
amarabuco <adrianomal@gmail.com>
2023-02-23 19:32:15 -0300

    sobrepreco

--- app/app/home.py
+++ app/app/home.py
@@ -1,20 +1,50 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import scipy as sp
+import plotly.express as px
 
 st.title('Mineração de Notas Fiscais')
 
-orgaos = pd.read_csv('../data/lista-de-orgaos-do-siafiLista-de-Orgaos-SIAFI.csv', encoding='utf-16', sep=',', on_bad_lines='skip', header=0)
-
-
-orgao = st.selectbox(
-    'Orgão',
-     orgaos['Órgão UGE Nome'],
-     index=23
+produto = st.selectbox(
+    'Produto',
+     ['Alcool Gel 500', 'Alcool 70', 'Vinagre']
      )
 
-if st.button('Ver notas'):
+preco = st.number_input('preço')
+quantidade = st.number_input('quantidade')
 
-    notas = pd.read_csv(f'../data/{orgao}.csv', header=0)
-    st.write(notas)
+if st.button('Calcular'):
+
+    notas = pd.read_csv(f'./data/alcool.csv', sep=';', header=0)
+    notas.Produto = notas.Produto.str.upper()
+    if produto == 'Alcool Gel 500':
+        notas = notas.loc[(notas.Produto.str.find(r'GEL') != -1)&(notas.Produto.str.find(r'500') != -1)&(notas['Unid. Medida'].str.find(r'UN') != -1)]
+
+    st.header('Intervalo')
+    stats = notas['Valor Unitário'].describe()
+    st.write(stats[['count','25%','mean','75%']].T)
+    fig = px.scatter(
+    notas,
+    x="Valor Unitário",
+    y="Quantidade",
+    # size='quantidade',
+    # color='descricaoProdutoServico',
+    # hover_data=['nomeFornecedor', 'descricaoProdutoServico'],
+    )
+    fig.update_layout(showlegend=False)
+
+    st.plotly_chart(fig, theme="streamlit")
+    
+    st.subheader('Intervalo de confiança:')
+    ic = sp.stats.norm.interval(0.95, loc=stats['mean'], scale=stats['std'])
+    st.write('Limite Inferior (LI):','{:.2f}'.format(ic[0]))
+    st.write( 'Limite Superior (LS): ','{:.2f}'.format(ic[1]))
+    st.write( 'Preço (P): ', str(preco), ' P > LS: ', str(preco > ic[1]) )
    
+    st.header('Sobrepreço')
+    if preco > ic[1]:
+        st.caption('Preço R$ {:.2f}'.format((preco - notas['Valor Unitário'].mean())))
+        st.caption('Valor R$ {:,.2f}'.format((preco - notas['Valor Unitário'].mean()).round(2) * quantidade))
+    else:
+        st.subheader('Não há evidência de sobrepreço.')
